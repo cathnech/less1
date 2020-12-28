2e9ea65ee4105cd7e34417360861579d4a103e14
cathnech <catherine.nech@gmail.com>
2020-12-28 14:03:48 +0300

    lesson1
    На работе в конце года ад, поэтому домашки с задержкой

--- src/less1/Main.java
+++ src/less1/Main.java
@@ -1,6 +1,4 @@
 package less1;
-
-import java.util.Arrays;
 import static java.util.Arrays.asList;
 
 
@@ -8,5 +6,42 @@ public class Main {
 
     public static void main(String[] args) {
 
+	    //задание 1 swap
+	    String  array1[] = {"a","b","c","d","e"};
+	    swap(array1,0,4);
+
+	    //задание 2
+	    String array2[] = {"A","B","C","D","E"};
+	    asList(array2);
+
+	    //задание 3
+
+	    Apple apple1 = new Apple();
+	    Apple apple2 = new Apple();
+	    Apple apple3 = new Apple();
+
+	    Orange orange1 = new Orange();
+	    Orange orange2 = new Orange();
+
+	    Box<Apple> box1 = new Box<Apple>(apple1, apple2, apple3);
+	    Box<Orange> box2 = new Box<Orange>(orange1, orange2);
+
+	    System.out.println(box1.compare(box2));
+
+	    Box<Orange> box3 = new Box<Orange>();
+	    box2.transfer(box3);
+
     }
+
+	private static void swap(String[]  array1, int i, int i1) {
+		if (i != i1) {
+			try {
+				String temp = array1[i];
+				array1[i] = array1[i1];
+				array1[i1] = temp;
+			} catch (IndexOutOfBoundsException e) {
+				System.out.println("oops");
+			}
+		}
+	}
 }
